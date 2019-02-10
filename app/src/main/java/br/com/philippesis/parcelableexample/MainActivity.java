2753f32a9096c36e662a3b5b19b98dd65078c16e
rogerphilippe@outlook.com <Um9nZXI=>
2019-02-10 21:42:49 -0200

    Concluído, passando objeto entre activitys.

--- app/src/main/java/br/com/philippesis/parcelableexample/MainActivity.java
+++ app/src/main/java/br/com/philippesis/parcelableexample/MainActivity.java
@@ -1,13 +1,47 @@
 package br.com.philippesis.parcelableexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int ano;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editNome = findViewById(R.id.id_edit);
+        final CalendarView calendarView = findViewById(R.id.id_calendar);
+        Button btnVerificar = findViewById(R.id.id_btn_verificar);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                ano = year;
+            }
+        });
+
+        btnVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Pessoa p = new Pessoa(editNome.getText().toString(), ano);
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                intent.putExtra("pessoa", p);
+
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
